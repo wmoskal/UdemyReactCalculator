43d2e40c8590e7b96591678f0ad674c495a921dd
wil moskal <wmoskal@uoguelph.ca>
2019-08-17 11:07:49 -0400

    Button Class implemented, incorrect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-
+import Button from './components/Button'
 class App extends Component{
     constructor(props) {
         super(props);
 
         this.state = {
-            total: 0
+            current: '0',
+            previous: []
         }
     }
 
+    reset = () => {
+        this.setState({ result: '0' });
+    }
+
+    addToCurrent = (symbol) => {
+        this.setState({ current: this.state.current + symbol });
+    }
+
     render() {
+        const buttons = [
+            {symbol: 'C', cols: 3, action: this.reset},
+            { symbol: '/', cols: 1, action: this.addToCurrent },
+            { symbol: '7', cols: 1, action: this.addToCurrent },
+            { symbol: '8', cols: 1, action: this.addToCurrent },
+            { symbol: '9', cols: 1, action: this.addToCurrent },
+            { symbol: 'X', cols: 1, action: this.addToCurrent },
+            { symbol: '4', cols: 1, action: this.addToCurrent },
+            { symbol: '5', cols: 1, action: this.addToCurrent },
+            { symbol: '6', cols: 1, action: this.addToCurrent },
+            { symbol: '-', cols: 1, action: this.addToCurrent },
+            { symbol: '1', cols: 1, action: this.addToCurrent },
+            { symbol: '2', cols: 1, action: this.addToCurrent },
+            { symbol: '3', cols: 1, action: this.addToCurrent },
+            { symbol: '+', cols: 1, action: this.addToCurrent },
+            { symbol: '0', cols: 2, action: this.addToCurrent },
+            { symbol: '.', cols: 1, action: this.addToCurrent },
+            { symbol: '=', cols: 1, action: this.addToCurrent },
+        ];
         let { count } = this.state;
         return (
-            <div id='container'>
-                <h1>Calculator</h1>
-                <br><br>
-                
+            <div className='app'>
+                <input className='result' type='text' value={this.state.current} />
 
+                {buttons.map((btn, i) => {
+                    return <Button symbol={btn.symbol} cols={btn.cols} action={(symmbol) => btn.action} />
+                })}
 
             </div>
         );
